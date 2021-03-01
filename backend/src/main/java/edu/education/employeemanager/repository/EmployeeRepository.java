4ea08e8b29043b9ae89a825bf3daf2e935193d1d
hgxey6 <hgxey6@gmail.com>
2021-03-01 18:16:10 +0500

    adding repository

---
+++ backend/src/main/java/edu/education/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package edu.education.employeemanager.repository;
+
+import edu.education.employeemanager.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
