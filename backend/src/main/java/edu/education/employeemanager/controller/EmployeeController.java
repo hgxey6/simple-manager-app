389c30dd9dff4d5e49f4593f498f1d02c19808d5
hgxey6 <hgxey6@gmail.com>
2021-03-01 18:21:48 +0500

    adding 'EmployeeController'

---
+++ backend/src/main/java/edu/education/employeemanager/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package edu.education.employeemanager.controller;
+
+import edu.education.employeemanager.domain.Employee;
+import edu.education.employeemanager.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    private final EmployeeService service;
+
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployee() {
+        List<Employee> employees = service.findAllEmployee();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        Employee employee = service.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = service.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee updateEmployee = service.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
+        service.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
